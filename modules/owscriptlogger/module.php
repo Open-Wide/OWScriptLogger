570249021739b8bcb7231293c111ff992fa96948
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-09-16 11:56:21 +0200

    Remove graph function

--- modules/owscriptlogger/module.php
+++ modules/owscriptlogger/module.php
@@ -13,14 +13,5 @@ $ViewList['logs'] = array(
     'post_action_parameters' => array( 'Delete' => array( 'DeleteIDArray' => 'DeleteIDArray' ) )
 );
 
-$ViewList['graphs'] = array(
-    'script' => 'graphs.php',
-    'functions' => array( 'read' ),
-    'default_navigation_part' => 'owmigration',
-    'default_navigation_part' => 'owscriptlogger',
-    'ui_context' => 'view',
-    'params' => array( 'LoggerID' ),
-);
-
 $FunctionList['read'] = array( );
 ?>
