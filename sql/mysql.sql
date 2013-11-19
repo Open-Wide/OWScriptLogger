ccdba3eb1712a3da10bcb5fee6de54f4e06c543c
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-11-19 14:36:38 +0100

    Update mysql table creation script

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -21,3 +21,15 @@ ALTER TABLE  `owscriptlogger` ADD  `notice_count` INT NOT NULL DEFAULT  '0';
 ALTER TABLE  `owscriptlogger` ADD  `warning_count` INT NOT NULL DEFAULT  '0';
 ALTER TABLE  `owscriptlogger` ADD  `error_count` INT NOT NULL DEFAULT  '0';
 ALTER TABLE  `owscriptlogger` ADD  `status` varchar(100) NOT NULL DEFAULT  'running';
+
+ALTER TABLE owscriptlogger ADD COLUMN owscriptlogger_script_id int(11) NOT NULL DEFAULT '0';
+CREATE TABLE owscriptlogger_script (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    identifier varchar(100) NOT NULL,
+    database_log_level varchar(10) NOT NULL DEFAULT 'notive',
+    fatal_error_recipients text DEFAULT NULL,
+    max_age_error int DEFAULT '0',
+    max_age_finished int DEFAULT '0',
+    max_age_manually_stoped int DEFAULT '0',
+    PRIMARY KEY ( id )
+);
